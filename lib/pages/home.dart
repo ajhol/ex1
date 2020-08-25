ff5687bf00d0d62a428d44b0e1971112955a2f97
ajhol <abdloh@gmail.com>
2020-08-25 18:09:35 +0700

    initial commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('My klkllkFirst App  AppBar')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Hello'),
+            ],
+          ),
+          Text('data'),
+          Text('2d')
+        ],
+      ),
+      //  Column(
+      //    children: <Widget>[Text('data1'), Text('test'), Text('ทดลอง4')],
+    );
+  }
+}
